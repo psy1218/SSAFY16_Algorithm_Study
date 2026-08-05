0201ca8e7fb99c7778c7b850340f5ab1330e7c5c
genius1998 <effort98@naver.com>
2026-08-05 12:33:41 +0900

    segment default 오류 해결

--- 2주차_8_10/표 편집/ingyu.cpp
+++ 2주차_8_10/표 편집/ingyu.cpp
@@ -3,9 +3,12 @@
 #include <sstream>
 #include <iostream>
 #include <tuple>
+#include <cstring>
 
 using namespace std;
-
+int n = 8;
+int k = 2;
+vector<string> cmd = { "D 2","C","U 3","C","D 4","C","U 2","Z","Z" };
 string solution(int n, int k, vector<string> cmd) {
 	string answer = "";
 	for (int i = 0; i < n; i++) {
@@ -15,36 +18,29 @@ string solution(int n, int k, vector<string> cmd) {
 	int cur = k; // 현재 커서 위치
 	// down = [1, 2, 3 , ... , n-1, -1]
 	// up = [-1, 0, 1, 2, ... , n-1]
-	int down[1000];
-	int up[1000];
+    vector<int> down(n);
+    vector<int> up(n);
 	for (int i = 1; i <= n - 1; i++) {
 		down[i - 1] = i;
 	}
 	down[n - 1] = -1;
-	for (int i = 0; i <= n - 1; i++) {
+	for (int i = 0; i < n - 1; i++) {
 		up[i + 1] = i;
 	}
 	up[0] = -1;
 	for (int i = 0; i < cmd.size(); i++) {
-		string str = cmd[i];
-		stringstream ss(str);
-		string word;
-
-		vector<string> result;
-		while (ss >> word) {
-			result.push_back(word);
-		}
-		if (result.size() == 2) {
+        char command = cmd[i][0];
+		if (command == 'U' || command == 'D') {
 			// cout << result[0] << " " << result[1] << endl;
-			int move_size = stoi(result[1]);
+			int move_size = stoi(cmd[i].substr(2));
 			// cout << move_size << "움직인 크기" << endl;
-			if (result[0] == "D") {
+			if (command == 'D') {
 				for (int j = 0; j < move_size; j++) {
 					cur = down[cur];
 					// cout << cur << "로 내려감" << endl;
 				}
 			}
-			else if (result[0] == "U") {
+			else if (command == 'U') {
 				for (int j = 0; j < move_size; j++) {
 					cur = up[cur];
 					// cout << cur << "로 올라감" << endl;
@@ -53,7 +49,7 @@ string solution(int n, int k, vector<string> cmd) {
 		}
 		else { // 삭제/복구
 			// cout << result[0] << endl;
-			if (result[0] == "C") { // 삭제. 복구를 위해서, 스택을 이용해서 최근에 삭제한 인덱스일수록 위에 존재하게 해야함
+			if (command == 'C') { // 삭제. 복구를 위해서, 스택을 이용해서 최근에 삭제한 인덱스일수록 위에 존재하게 해야함
 				// cout << cur << "번째를 삭제함" << endl;
 				answer[cur] = 'X';
 				if (down[cur] == -1) { // 현재 커서 위치가 마지막이라면
@@ -64,9 +60,9 @@ string solution(int n, int k, vector<string> cmd) {
 				else {
 					deleted.push_back({ cur, down[cur], up[cur] });
 					int temp = up[cur];
-					cur = down[cur]; // 다음으로 이동한 다음에
-					down[temp] = cur; // 이전의 커서를 다음으로 이동된 현재 커서의 위치로 이동시킬 수 있게함.
-					up[cur] = temp;
+                    cur = down[cur]; 
+                    if (temp != -1) down[temp] = cur; // temp가 -1이 아닐 때만 접근하도록 수정
+                    up[cur] = temp;
 				}
 			}
 			else { // 복구
@@ -88,4 +84,8 @@ string solution(int n, int k, vector<string> cmd) {
 		// cout << "현재 cur 위치:" << cur << endl;
 	}
 	return answer;
+}
+int main() {
+	cout << solution(n, k, cmd);
+	return 0;
 }
