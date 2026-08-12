fa816d4c9616a71123a07b98819afa29ddb26b8b
genius1998 <effort98@naver.com>
2026-08-12 17:09:23 +0900

    Refactor code to implement route counting solution
    Refactored the code to implement a new solution for counting routes. Removed unused includes and main function.

--- 4주차_8_24/단속카메라/ingyu.cpp
+++ 4주차_8_24/단속카메라/ingyu.cpp
@@ -1,39 +1,36 @@
 #include <string>
 #include <vector>
-#include <sstream>
-#include <iostream>
-#include <tuple>
-#include <cstring>
+#include <deque>
 #include <algorithm>
-#include <queue>
-#include <stack>
-#include <unordered_map>
-#include <iomanip>
+
 using namespace std;
-int main() {
-	int TC;
-	cin >> TC;
-	for (int i = 1; i <= TC; i++) {
-		float n, k;
-		cin >> n >> k;
-		priority_queue<float, vector<float>, greater<float>> pq;
-		int limit = n - k; // 최대 limit갯수만큼 빼야함
-		for (int j = 0; j < n; j++) {
-			float a;
-			cin >> a;
-			pq.push(a);
-		}
-		while (limit != 0) {
-			pq.pop();
-			limit--;
-		}
-		float cur = 0; // 성수의 실력
-		while (!pq.empty()) {
-			float num = pq.top();
-			pq.pop();
-			cur = (cur + num) / 2;
-		}
-		cout << "#" << i << " " << fixed << setprecision(6) << cur << endl;
-	}
-	return 0;
+
+int solution(vector<vector<int>> routes) {
+    sort(routes.begin(), routes.end(), [](const vector<int>& a, const vector<int>& b) {
+        return a[1] < b[1];
+    });
+
+    deque<vector<int>> q(routes.begin(), routes.end());
+
+    int cnt = 0;
+
+    while (!q.empty()) {
+        vector<int> cur_car = q.front();
+        q.pop_front();
+
+        int cur_start_time = cur_car[1];
+
+        if (q.empty()) {
+            cnt++;
+            break;
+        }
+
+        while (!q.empty() && q.front()[0] <= cur_start_time) {
+            q.pop_front();
+        }
+
+        cnt++;
+    }
+
+    return cnt;
 }
