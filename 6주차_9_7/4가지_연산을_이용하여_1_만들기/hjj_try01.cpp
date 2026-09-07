8f51c855f1f327418dfd36ded6a506b1c1e31f64
Yuris <mose1207@gmail.com>
2026-09-07 11:07:09 +0900

    정해진 코드트리 4가지 연산을 이용하여 1 만들기, k번 최댓값으로 이동하기 제출

---
+++ 6주차_9_7/4가지_연산을_이용하여_1_만들기/hjj_try01.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int N;
+
+int main() {
+    cin >> N;
+
+    queue<pair<int, int>> procedure;
+    vector<int> saving_min_times(N + 1, -1);
+    procedure.push({ N, 0 });
+
+    int curr_num;
+    int next_num;
+    int curr_calculate_times = 0;
+
+    while (!procedure.empty()) {
+        curr_num = procedure.front().first;
+        curr_calculate_times = procedure.front().second;
+        procedure.pop();
+
+        if (curr_num == 1) break;
+
+        // 현재 수가 3의 배수인 경우 3으로 나누기
+        if (curr_num % 3 == 0) {
+            next_num = curr_num / 3;
+            if (saving_min_times[next_num] != -1 && saving_min_times[next_num] < curr_calculate_times + 1) continue;
+            else {
+                saving_min_times[next_num] = curr_calculate_times + 1;
+                procedure.push({ next_num, curr_calculate_times + 1 });
+            }
+        }
+        // 현재 수가 2의 배수인 경우 2로 나누기
+        if (curr_num % 2 == 0) {
+            next_num = curr_num / 2;
+            if (saving_min_times[next_num] != -1 && saving_min_times[next_num] < curr_calculate_times + 1) continue;
+            else {
+                saving_min_times[next_num] = curr_calculate_times + 1;
+                procedure.push({ next_num, curr_calculate_times + 1 });
+            }
+        }
+        // 현재 수에서 1을 빼기
+        if (curr_num > 0) {
+            next_num = curr_num - 1;
+            if (saving_min_times[next_num] != -1 && saving_min_times[next_num] < curr_calculate_times + 1) continue;
+            else {
+                saving_min_times[next_num] = curr_calculate_times + 1;
+                procedure.push({ next_num, curr_calculate_times + 1 });
+            }
+        }
+        // 현재 수에 1을 더하기
+        if (curr_num < N) {
+            next_num = curr_num + 1;
+            if (saving_min_times[next_num] != -1 && saving_min_times[next_num] < curr_calculate_times + 1) continue;
+            else {
+                saving_min_times[next_num] = curr_calculate_times + 1;
+                procedure.push({ next_num, curr_calculate_times + 1 });
+            }
+        }
+    }
+
+    cout << curr_calculate_times;
+}
